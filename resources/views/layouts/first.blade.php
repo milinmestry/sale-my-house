c62e56ddf81cb82d69ac4eb4784615bd047c7205
Milin Mestry <milinmestry@gmail.com>
2018-03-12 23:24:42 +0530

    Header, footer added in the layout file.

--- resources/views/layouts/first.blade.php
+++ resources/views/layouts/first.blade.php
@@ -1,60 +1,52 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <title>@yield('title')</title>
+  <head>
+    <title>@yield('title')</title>
 
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-    <body>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    <main>
       <header>
-        <div class="menu w-100 w-90-l ph3 ph0-l center flex items-center justify-between contain">
-          <a href="/">
-            logo
-          </a>
-
-          <button type="button" class="db dn-l bn menuButton bg-transparent ">
-            <svg width="25px" height="18px" viewBox="0 3 25 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-              <g id="menuIcon" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" transform="translate(0.000000, 3.000000)">
-              <rect class="hamburger" id="bottom" fill="
-                #1D1D1D        " x="0" y="16" width="24" height="2"></rect>
-              <rect class="hamburger" id="mid" fill="
-                #1D1D1D        " x="0" y="8" width="24" height="2"></rect>
-              <rect class="hamburger" id="top" fill="
-                #1D1D1D        " x="0" y="0" width="24" height="2"></rect>
-              </g>
+        <nav class="flex justify-between bb b--white-10 bg-mid-gray">
+          <a class="link white-70 hover-white no-underline flex items-center pa3" href="">
+            <svg
+              class="dib h1 w1"
+              data-icon="grid"
+              viewBox="0 0 32 32"
+              style="fill:currentcolor">
+              <title>Super Normal Icon Mark</title>
+              <path d="M2 2 L10 2 L10 10 L2 10z M12 2 L20 2 L20 10 L12 10z M22 2 L30 2 L30 10 L22 10z M2 12 L10 12 L10 20 L2 20z M12 12 L20 12 L20 20 L12 20z M22 12 L30 12 L30 20 L22 20z M2 22 L10 22 L10 30 L2 30z M12 22 L20 22 L20 30 L12 30z M22 22 L30 22 L30 30 L22 30z">
+              </path>
             </svg>
-          </button>
-
-          <nav class="big-nav dn db-l">
-            <a class="ml4 b" href="https://interfacelovers.com/interviews">Interviews</a>
-            <a class="ml4 b" href="https://interfacelovers.com/mixes">Mixes</a>
-            <a class="ml4 b" href="https://interfacelovers.com/about">About</a>
-              <button type="button" class="ml4 b subscribeButton">Subscribe</button>
-          </nav>
-        </div>
+          </a>
+          <div class="flex-grow pa3 flex items-center">
+            <a class="f6 link dib white dim mr3 mr4-ns" href="#0">About</a>
+            <a class="f6 link dib white dim mr3 mr4-ns" href="#0">Sign In</a>
+            <a class="f6 dib white bg-animate hover-bg-white hover-black no-underline pv2 ph4 br-pill ba b--white-20" href="#0">Sign Up</a>
+          </div>
+        </nav>
       </header>
-        @section('sidebar')
-            This is the master sidebar.
-        @show
+      
+      <div class="center w-100 db cf bg-lightest-blue">
+        @yield('content')
 
-        <div class="container">
-            @yield('content')
-        </div>
-
-        <footer class="b tc w-100 white pv4 pv5-l">
+        <footer class="b tc w-100 pv1 pv2-l bg-mid-gray">
           <ul class="center dib list">
-                        <li class="dib-l mb4 mb2-l"><a href="https://interfacelovers.com/interviews">Interviews</a>
-              </li><li class="dn dib-l ph2">/</li>
-                    <li class="dib-l mb4 mb2-l"><a href="https://interfacelovers.com/mixes">Mixes</a>
-              </li><li class="dn dib-l ph2">/</li>
-                    <li class="dib-l mb4 mb2-l"><a href="https://interfacelovers.com/about">About</a>
-              </li><li class="dn dib-l ph2">/</li>
-                      <li class="dib-l mb4 mb2-l"><a href="mailto:hi@interfacelovers.com?subject=Post%20Job" target="_blank">Post Job</a></li>
-            <li class="dn dib-l ph2">/</li>
-            <li class="dib-l"><a href="mailto:hi@interfacelovers.com" target="_blank">Email Us</a></li>
+            <li class="dib-l mb4 mb2-l pa2"><a class="white" href="https://interfacelovers.com/interviews">Login</a>
+            </li>
+            <li class="dib-l mb4 mb2-l pa2"><a class="white" href="https://interfacelovers.com/mixes">Register</a>
+            </li>
+                  
           </ul>
         </footer>
-    </body>
+      </div>
+
+
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+        <link rel="stylesheet" href="{{ mix('/css/tachyons.min.css') }}">
+    </main>
+  </body>
 </html>
