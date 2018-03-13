f3954418b1a035f42ad42d3d249c8ceacf279cda
Milin Mestry <milinmestry@gmail.com>
2018-03-13 13:02:50 +0530

    Home page header, footer layout updated. Favicon icon added.

--- resources/views/layouts/first.blade.php
+++ resources/views/layouts/first.blade.php
@@ -2,50 +2,50 @@
 <html lang="{{ app()->getLocale() }}">
   <head>
     <title>@yield('title')</title>
+    <link rel="shortcut icon" type="image/png" href="/images/icons/building-2.png"/>
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
-  <body>
+  <body class="sans-serif">
     <main>
       <header>
-        <nav class="flex justify-between bb b--white-10 bg-mid-gray">
-          <a class="link white-70 hover-white no-underline flex items-center pa3" href="">
-            <svg
-              class="dib h1 w1"
-              data-icon="grid"
-              viewBox="0 0 32 32"
-              style="fill:currentcolor">
-              <title>Super Normal Icon Mark</title>
-              <path d="M2 2 L10 2 L10 10 L2 10z M12 2 L20 2 L20 10 L12 10z M22 2 L30 2 L30 10 L22 10z M2 12 L10 12 L10 20 L2 20z M12 12 L20 12 L20 20 L12 20z M22 12 L30 12 L30 20 L22 20z M2 22 L10 22 L10 30 L2 30z M12 22 L20 22 L20 30 L12 30z M22 22 L30 22 L30 30 L22 30z">
-              </path>
-            </svg>
+        <nav class="flex justify-between bb b--white-10 bg-navy o-70">
+          <a class="link white-70 hover-white no-underline flex items-center pa2" href="/">
+            <img src="/images/icons/building-1.png" class="bg-white-80 pa1" title="Home">
           </a>
           <div class="flex-grow pa3 flex items-center">
-            <a class="f6 link dib white dim mr3 mr4-ns" href="#0">About</a>
-            <a class="f6 link dib white dim mr3 mr4-ns" href="#0">Sign In</a>
-            <a class="f6 dib white bg-animate hover-bg-white hover-black no-underline pv2 ph4 br-pill ba b--white-20" href="#0">Sign Up</a>
+          @if (Route::has('login'))
+            @auth
+              <a class="f6 link dib white dim mr3 mr4-ns" href="{{ url('/home') }}">Home</a>
+            @else
+              <a class="f6 link dib white dim mr3 mr4-ns" href="{{ route('login') }}">Sign In</a>
+              <a class="f6 dib white bg-animate hover-bg-white hover-black no-underline pv2 ph4 br-pill ba b--white-20" href="{{ route('register') }}">Sign Up</a>
+            @endauth
+          @endif
           </div>
         </nav>
       </header>
-      
-      <div class="center w-100 db cf bg-lightest-blue">
-        @yield('content')
 
-        <footer class="b tc w-100 pv1 pv2-l bg-mid-gray">
-          <ul class="center dib list">
-            <li class="dib-l mb4 mb2-l pa2"><a class="white" href="https://interfacelovers.com/interviews">Login</a>
-            </li>
-            <li class="dib-l mb4 mb2-l pa2"><a class="white" href="https://interfacelovers.com/mixes">Register</a>
-            </li>
-                  
-          </ul>
-        </footer>
+      <div class="center w-100 db cf bg-light-gray ph3">
+        @yield('content')
       </div>
 
+      <footer class="tc w-100 bg-navy o-70">
+      @if (Route::has('login'))
+        <ul class="center dib list">
+          <li class="dib-l mb4 mb2-l pa2"><a class="white" href="{{ route('login') }}">Sign In</a>
+          </li>
+          <li class="dib-l mb4 mb2-l pa2"><a class="white" href="{{ route('register') }}">Sign Up</a>
+          </li>
+        </ul>
+      @endif
+      </footer>
 
-        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+
+
+        <!-- <link rel="stylesheet" href="{{ mix('/css/app.css') }}"> -->
         <link rel="stylesheet" href="{{ mix('/css/tachyons.min.css') }}">
     </main>
   </body>
