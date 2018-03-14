f221924c08350da9e6f50bd3df1bd3f201f4a8db
Milin Mestry <milinmestry@gmail.com>
2018-03-14 18:19:42 +0530

    Seller Registration form is created. Functionality pending.

--- resources/lang/en/forms.php
+++ resources/lang/en/forms.php
@@ -1,5 +1,7 @@
 <?php
 return [
+  'BUTTON_SAVE' => 'Save',
+  'BUTTON_CANCEL' => 'Cancel',
   'REGISTRATION' => [
     'SIGN_UP' => 'Sign Up',
     'SIGN_IN' => 'Sign In',
@@ -8,5 +10,12 @@ return [
     'PASSWORD' => 'Password',
     'CONFIRM_PASSWORD' => 'Confirm Password',
     'MOBILE' => 'Mobile Number',
+  ],
+  'REG_SELLER' => [
+    'HEADING' => 'Become A Seller',
+    'PRESENT_ADDRESS' => 'Present Address',
+    'PERMANENT_ADDRESS' => 'Permanent Address',
+    'CONTACT_LANDLINE' => 'Contact Number',
+    'ALTERNATE_E-MAIL' => 'Additional E-mail',
   ],
 ];
